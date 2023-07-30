c4962f55714e833531f207d65f97f2ad4eb5b6c1
duncrob <duncrob@uw.edu>
2023-07-30 16:42:44 -0700

    Added sorting implementation for arrays of numbers and strings utilizing an interface

--- sort/build/index.js
+++ sort/build/index.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log('hi there');
+Object.defineProperty(exports, "__esModule", { value: true });
+const Sorter_1 = require("./Sorter");
+const CharactersCollection_1 = require("./CharactersCollection");
+// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+const charactersCollection = new CharactersCollection_1.CharactersCollection('Xaayb');
+const sorter = new Sorter_1.Sorter(charactersCollection);
+sorter.sort();
+console.log(charactersCollection.data);
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,1 +1,13 @@
-console.log('hi there');
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
+
+// const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+// const sorter = new Sorter(numbersCollection);
+// sorter.sort();
+// console.log(numbersCollection.data);
+
+const charactersCollection = new CharactersCollection('Xaayb');
+const sorter = new Sorter(charactersCollection);
+sorter.sort();
+console.log(charactersCollection.data);
