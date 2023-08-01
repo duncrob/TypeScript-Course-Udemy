e8b128417dbffbb023a1a6c03e6b8990e889f00b
duncrob <duncrob@uw.edu>
2023-08-01 11:17:09 -0700

    Finished rendering front end through our own web framework

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,7 +1,15 @@
-import { UserForm } from './views/UserForm';
+import { UserEdit } from './views/UserEdit';
 import { User } from './models/User';
 
 const user = User.buildUser({ name: 'NAME', age: 20 });
 
-const userForm = new UserForm(document.getElementById('root')!, user);
-userForm.render();
+const root = document.getElementById('root');
+
+if (root) {
+  const userEdit = new UserEdit(root, user);
+  userEdit.render();
+
+  console.log(userEdit);
+} else {
+  throw new Error('Root element not found');
+}
